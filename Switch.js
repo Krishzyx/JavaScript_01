f05a4b4599ddb0d7f4174067455ac1c183a19c5e
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2023-02-03 23:20:11 +0530

    Create Switch.js

---
+++ Switch.js
@@ -0,0 +1,17 @@
+const expr = 'mangoes ' ;
+switch (expr){
+  case 'oranges':
+    console.log("ogranges are 40rs/kg");
+    break;
+  case 'guava':
+    console.log("guavaes are 30rs/kg");
+    break;  
+    case 'mangoes':
+    console.log("mangoes are 50rs/kg");
+    break;  
+    case 'apples':
+    console.log("apples are 35rs/kg");
+    break; 
+    default :
+    console.log("sorry we are out of your (expr)")
+}
