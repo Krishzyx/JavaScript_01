3eb570ccc9cd59a6823b3a539f74cdac7534c7ee
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2023-02-03 23:22:37 +0530

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,13 @@
+//template literls
+let boy1 = "prince";
+let boy2 = "dipanshu";
+
+let sentence = `${boy2} is freind of ${boy1}`
+console.log(sentence)
+
+
+//escape sequence charcters
+let fruit = 'bana\'na'
+console.log(fruit)
+console.log(fruit.length)//its only gives 7 in output because backslash and single quotes take as one character
+
