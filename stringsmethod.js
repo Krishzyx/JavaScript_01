7f55d2f7e2b67ee547bfba83261ffca5d9c74434
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2023-02-03 23:23:38 +0530

    Create stringsmethod.js

---
+++ stringsmethod.js
@@ -0,0 +1,15 @@
+let name = "prince"
+//this gives you  length of the string
+console.log(name.length);
+//this returns the string to uppercase
+console.log(name.toUpperCase());
+//this returns the string to lower case
+console.log(name.toLowerCase());
+//this one slice the string 
+console.log(name.slice(2,4));
+//this one slice the string at teh index 2
+console.log(name.slice(2));
+//this one replaced the string 
+console.log(name.replace("pri","kri"));
+//this string method returns the addition of strings 
+console.log(name.concat(" maurya"))
