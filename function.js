030c07397199929645cfeb58fd1de9a1c063b557
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2023-02-03 23:21:37 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,9 @@
+unction onePlusAvg(x,y){
+  return 1+ (x+y)/2
+}
+ let a = 2;
+let b = 5;
+let c = 8;
+ console.log("one plus Average Of a and b ", onePlusAvg(a,b))
+console.log("one plus Average Of b and c ", onePlusAvg(b,c))
+console.log("one plus Average Of c and a ", onePlusAvg(c,a))
